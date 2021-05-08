8f06bfca501d3fd7f1e869a312bd1ab8b4c26f26
usrfriendly <arinlares@gmail.com>
2021-05-07 17:52:31 -0700

    Added numpad

--- keymap.c
+++ keymap.c
@@ -6,8 +6,24 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
-//enum custom_keycodes {
-//};
+enum custom_keycodes {
+        // a list, so if any get added, use commas
+        HUNIT,
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+        switch(keycode) {       
+                case HUNIT:     
+                        if (record->event.pressed) {
+                                // activates HUND 
+                                SEND_STRING("00");
+                        }
+                        else {
+                                // do nothing
+                        }
+                        break;
+        }
+        return true;
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -25,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  MO(4),   KC_LCTL, KC_LALT, KC_LGUI, MO(2),   KC_SPC,  KC_SPC,  MO(3),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  MO(5),   KC_LCTL, KC_LALT, KC_LGUI, MO(2),   KC_SPC,  KC_SPC,  MO(3),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Gaming (space preserved)
@@ -46,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [1] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   KC_TRNS, KC_LCTL, KC_LALT, KC_SPC, KC_SPC,   KC_SPC,  MO(2),   MO(3),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
@@ -71,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   BL_STEP, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME,KC_END, KC_MPLY, \
-  _______, _______, _______, _______, _______, _______, MO(4), _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
+  _______, _______, _______, _______, _______, _______, _______, MO(4),  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
 ),
 
 /* Raise
@@ -114,8 +130,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+),
+
+
+//theoretical numpad layout, may get function keys, [5] to establish numpad numbers and control layout
+/* Bottom/leftmost button layer
+ *                      
+ * ,-----------------------------------------------------------------------------------.
+ * |NORMAL|      |      |      |      |      |      |      | KC_P7| KC_P8| KC_P9|      | 
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |GAMING|      |      |      |      |      |      |      | KC_P4| KC_P5| KC_P6|      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | KC_P1| KC_P2| KC_P3|      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | KC_P0| KC_00|KC_DOT|      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[5] = LAYOUT(
+    DF(0),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, 
+    DF(1),   _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_DEL,
+    _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
+    _______, MO(6),   _______, _______, _______, _______, _______, KC_P0,   HUNIT,   KC_PDOT, _______, _______
+),
+//theoretical numpad layout, higher layer, [5] for inputting/functions, etc
+/* Adjust (Lower + Raise)
+ *                      
+ * ,-----------------------------------------------------------------------------------.
+ * |NORMAL|      |      |      |      |      |      |      |      |      |      |      | 
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |GAMING|      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[6] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, 
+    _______, _______, _______, _______, _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______, KC_DEL,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PPLS, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PENT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
-
-
 };
-
